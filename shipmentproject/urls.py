3c7b8fbdccf78e1623f5a00d5a8572794e086b58
ecedreamer <sangit.niroula@gmail.com>
2020-03-02 12:19:57 +0545

    ASDF

--- shipmentproject/urls.py
+++ shipmentproject/urls.py
@@ -4,9 +4,9 @@ from django.conf import settings
 from django.contrib import admin
 from shipmentapp.views import Demoview
 urlpatterns = [
-    re_path('^.*',Demoview.as_view(),name="demo"),
+    # re_path('',Demoview.as_view(),name="demo"),
     path('django-admin/', admin.site.urls),
-    path('home/', include('shipmentapp.urls')),
+    path('', include('shipmentapp.urls')),
 
 ]
 
